8435a9d3bd5569b6d06e921cecb493a64f6e7157
tergytrg <73442558+tergytrg@users.noreply.github.com>
2022-11-23 20:01:00 +0100

    Nieuw commando

---
+++ src/commands/admin/reset.js
@@ -0,0 +1,42 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const init = require('../../init-list');
+
+// Als er een oude initiative in de chat staat, kun je deze snel toevoegen met dit commando
+// Voorbeeld input:
+// Initiative gestart: Doolan: 20 Rivian: 20 lair: 20 Bob: 16 Stormspawn: 9 Heldras: 9 Tacetti: 6 capuel: 5 Riza: 2
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('reset')
+    .setDescription('Gebruik een oude initiative lijst')
+
+    .addStringOption(option => option
+        .setName('oud')
+        .setDescription('Je kunt gewoone een compleet bericht uit #initiative hierin plakken')
+        .setRequired(true)),
+
+    async execute(interaction, client) {
+    
+        if (!init.isFresh()) {
+            await interaction.reply({content: "**Pas op!**\nJe bent nu rolls aan een oude initiative aan het toevoegen. Je kunt een nieuwe initiative maken met !new"});
+        }
+
+        let naam = interaction.options.getString('oud');
+        const oldInits = naam.split(" ");
+        for (let i = 2; i < oldInits.length; i += 2) {
+            let naam = oldInits[i];
+            let j = i + 1;
+            while(j < oldInits.length && !Number.isInteger(Number(oldInits[j]))) {
+                naam += " " + oldInits[j];
+                j++;
+            }
+            const rol = parseInt(oldInits[j]);
+            naam = naam.slice(0, -1);
+            console.log("naam " + naam);
+            console.log("rol " + rol);
+            init.custom(i, rol - i, naam)
+        }
+
+        await interaction.reply({content: "Oude initiative gestuurd!"});
+        init.update();
+    },
+}
